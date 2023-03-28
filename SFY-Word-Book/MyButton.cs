95b39d6a8a04ac853c8d5dc022bb879cc2d14686
书封影 <ShuFengYingt@outlook.com>
2023-03-28 20:18:52 +0800

    x:Type
    This time I tried to make a DIY button class named MyButton and override the OnClick() method to make it can activate MyWindow  which was being created in the solution.
    And I also found a perfect software to design the UI which is named Blend.

---
+++ SFY-Word-Book/MyButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SFY_Word_Book
+{
+    class MyButton : Button
+    {
+        public Type UserWindowType { get; set; }
+
+        //重写按键触发
+        protected override void OnClick()
+        {
+            base.OnClick();//当基类触发该事件
+            Window win = Activator.CreateInstance(this.UserWindowType) as Window; //获取用户按钮类型
+            
+            //default
+            if (win != null)
+            {
+                win.ShowDialog();
+            }
+        }
+    }
+}
