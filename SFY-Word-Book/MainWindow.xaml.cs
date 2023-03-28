4093ad73a45c0c0589332d925aa91ab214dedfe8
书封影 <ShuFengYingt@outlook.com>
2023-03-28 15:40:45 +0800

    Key-Value with XAML and Resources

--- SFY-Word-Book/MainWindow.xaml.cs
+++ SFY-Word-Book/MainWindow.xaml.cs
@@ -25,24 +25,10 @@ namespace SFY_Word_Book
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            StackPanel stackPanel = this.Content as StackPanel;
-            TextBox textBox = stackPanel.Children[0] as TextBox;
-
-            if (string.IsNullOrEmpty(textBox.Name))
-            {
-                textBox.Text = "No name!";
-            }
-            else
-            {
-                textBox.Text = textBox.Name;
-            }
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            string str = this.FindResource("myString") as string;
+            this.textbox_1.Text = str;
         }
     }
 }
