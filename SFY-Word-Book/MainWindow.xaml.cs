3bf2c6288a0f4ca1d5c348fddec83daaa4197c38
书封影 <ShuFengYingt@outlook.com>
2023-03-28 14:50:47 +0800

    A button and a textbox interact together
    Finish a new Node of the WPF learning pathway.

--- SFY-Word-Book/MainWindow.xaml.cs
+++ SFY-Word-Book/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace SFY_Word_Book
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            StackPanel stackPanel = this.Content as StackPanel;
+            TextBox textBox = stackPanel.Children[0] as TextBox;
+
+            if (string.IsNullOrEmpty(textBox.Name))
+            {
+                textBox.Text = "No name!";
+            }
+            else
+            {
+                textBox.Text = textBox.Name;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
