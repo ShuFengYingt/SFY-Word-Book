e3c2cadb9d7ad77b98740e36b52f11025cb17859
书封影 <ShuFengYingt@outlook.com>
2023-03-30 19:52:52 +0800

    INotifyPropertyChanged
    Seal base class.
    [CallerMemberName]
    
    This node of the learning pathway helps me to learn more knowledge about how to make code clearer and how to improve its readability.

--- SFY-Word-Book/MainViewModel.cs
+++ SFY-Word-Book/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,18 +9,49 @@ using System.Windows;
 
 namespace SFY_Word_Book
 {
-    public class MainViewModel
+    
+    public class MainViewModel :ViewModelBase
     {
         public MainViewModel()
         {
             ShowCommand = new MyCommand(Show);
+            Name = "Hello";
+            Title = "Title";
+        }
+
+        private string name;
+        private string title;
+
+        public string Name 
+        {
+            get { return name; }
+            set 
+            { 
+                name = value;
+
+                //ViewModelBase下的通知更新方法
+                OnPropertyChanged();
+            }
+
+        }
+
+        //如法炮制
+        public string Title
+        {
+            get { return title; }
+            set { title = value;OnPropertyChanged(); }
         }
 
         public MyCommand ShowCommand { get; set; }
 
+
         public void Show()
         {
+            Name = "Is Changed";
+            Title = "this is title";
             MessageBox.Show("asd");
         }
+
+
     }
 }
---
+++ SFY-Word-Book/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFY_Word_Book
+{
+    /// <summary>
+    /// 通知更新封装类
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// 传入属性名称，实现value更新
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")//CallerMemberName自动获取调用方法或者属性的名称并为该参数赋值，
+                                                                                 //需要设置defualt，这里以""为default
+                                                                                 //这样调用的时候就不需要传入属性的名字了
+                                                                                 //非常优雅的方法
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
