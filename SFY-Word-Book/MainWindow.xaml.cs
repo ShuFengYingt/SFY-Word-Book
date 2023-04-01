40c8dedfff2c3bbbcb986b0b116de0fe509d24c0
书封影 <ShuFengYingt@outlook.com>
2023-04-01 14:41:01 +0800

    A preliminary exploration of the DoubleAnimation.
    Essential animation in WPF.
    This step I create a button and make it changing its scale while the user click it.
    We need to use the DoubleAnimation and set the property of the animation and then use BeginAnimation to start it.

--- SFY-Word-Book/MainWindow.xaml.cs
+++ SFY-Word-Book/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Media.Animation;
 namespace SFY_Word_Book
 {
     /// <summary>
@@ -25,13 +25,32 @@ namespace SFY_Word_Book
         {
             InitializeComponent();
 
-          
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_1_Click(object sender, RoutedEventArgs e)
         {
-            this.Resources["SolidColor"] = new SolidColorBrush(Colors.AliceBlue);
+            /*这一部分声明动画的内容*/
+            //声明双精度 动画
+            DoubleAnimation animation = new DoubleAnimation();
+            //根据BeginAnimation设置的依赖属性，在原有基础上-30，
+            animation.By = -30;
+
+            ////设置动画初始值为按钮宽度
+            //animation.From = Button_1.Width;
+            ////结束值为宽度-30
+            //animation.To = Button_1.Width - 30;
+
+            //持续两秒
+            animation.Duration = TimeSpan.FromSeconds(1);
+            //设置往返执行
+            animation.AutoReverse = true;
+            //设置执行周期
+            animation.RepeatBehavior = new RepeatBehavior(2);
+
+             
+            /* 执行动画,传入依赖属性以及动画实例 */
+            Button_1.BeginAnimation(Button.WidthProperty, animation);
         }
     }
 }
