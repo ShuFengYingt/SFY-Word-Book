c5c628dca5fea3314ec0783ebd50818f45d3f93d
书封影 <ShuFengYingt@outlook.com>
2023-04-01 15:24:03 +0800

    Create project based on prism framework.

--- SFY-Word-Book/App.xaml.cs
+++ SFY-Word-Book/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +13,23 @@ namespace SFY_Word_Book
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        //基于Prism框架的WPF，
+        //1。引入prism，
+        //2，于app。xaml中引入prism命名空间，删去StartUpUI（如果有）
+        //3，将app。cs的基类改为PrismApplication
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Window>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+
     }
 }
--- SFY-Word-Book/MainWindow.xaml.cs
+++ SFY-Word-Book/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,29 +27,5 @@ namespace SFY_Word_Book
 
         }
 
-        private void Button_1_Click(object sender, RoutedEventArgs e)
-        {
-            /*这一部分声明动画的内容*/
-            //声明双精度 动画
-            DoubleAnimation animation = new DoubleAnimation();
-            //根据BeginAnimation设置的依赖属性，在原有基础上-30，
-            animation.By = -30;
-
-            ////设置动画初始值为按钮宽度
-            //animation.From = Button_1.Width;
-            ////结束值为宽度-30
-            //animation.To = Button_1.Width - 30;
-
-            //持续两秒
-            animation.Duration = TimeSpan.FromSeconds(1);
-            //设置往返执行
-            animation.AutoReverse = true;
-            //设置执行周期
-            animation.RepeatBehavior = new RepeatBehavior(2);
-
-             
-            /* 执行动画,传入依赖属性以及动画实例 */
-            Button_1.BeginAnimation(Button.WidthProperty, animation);
-        }
     }
 }
