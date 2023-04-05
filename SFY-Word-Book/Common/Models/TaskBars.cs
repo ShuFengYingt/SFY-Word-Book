445cf1fce69fc4f98aeec41b82af9f3d7e1419c2
书封影 <ShuFengYingt@outlook.com>
2023-04-05 20:59:18 +0800

    Finish HomeView UI
    1.TaskBars
    2.Welcome Text
    3.Two Border, daily page and word search.
    4.Corner Window

---
+++ SFY-Word-Book/Common/Models/TaskBars.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFY_Word_Book.Common.Models
+{
+    /// <summary>
+    /// 任务栏
+    /// </summary>
+    public class TaskBars:BindableBase
+    {
+        /// <summary>
+        /// 任务栏构造函数
+        /// </summary>
+        public TaskBars()
+        {
+        
+        }
+
+        //图标
+        private string icon;
+        /// <summary>
+        /// 图标
+        /// </summary>
+        public string Icon { get; set; }
+
+        //标题
+        private string title;
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get;set; }
+
+        //数据信息
+        private string content;
+        /// <summary>
+        /// 数据信息
+        /// </summary>
+        public string Content { get { return content; } set { content = value; } }
+
+        //颜色
+        private string color;
+        /// <summary>
+        /// 背景颜色
+        /// </summary>
+        public string Color { get { return color; } set { color = value; } }
+
+        //用于触发事件的命名空间
+        private string nameSpace;
+        /// <summary>
+        /// 用于触发事件的命名空间
+        /// </summary>
+        public string NameSpace { get {  return nameSpace; } set {  nameSpace = value; } }
+
+    }
+
+}
--- SFY-Word-Book/ViewModels/HomeViewModel.cs
+++ SFY-Word-Book/ViewModels/HomeViewModel.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using SFY_Word_Book.Common.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SFY_Word_Book.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel:BindableBase
     {
-        public HomeViewModel() { }
+        public HomeViewModel() 
+        {
+            //初始化
+            TaskBars = new ObservableCollection<TaskBars>();
+            CreateTaskBar();
+        }
+
+        //任务栏的通知更新
+        private ObservableCollection<TaskBars> taskBars;
+        public ObservableCollection<TaskBars> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+
+        void CreateTaskBar()
+        {
+
+            TaskBars.Add(new TaskBars { Icon = "BookOpenPageVariant", Title = "待学习", Content = "2677", Color = "#48815a", NameSpace = "" });
+            TaskBars.Add(new TaskBars { Icon = "BookClock", Title = "待复习", Content = "0", Color = "#008184", NameSpace = "" });
+            TaskBars.Add(new TaskBars { Icon = "BookCheck", Title = "已学习", Content = "121", Color = "#965fa0", NameSpace = "" });
+            TaskBars.Add(new TaskBars { Icon = "Bookshelf", Title = "单词书", Content = "四级大纲词汇", Color = "#866e66", NameSpace = "" });
+
+        }
+
     }
 }
+    
